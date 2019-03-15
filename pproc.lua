e0bfb61a05675f5b709d465565d7a878a479859f
Alexander Petrov <alpet1@gmail.com>
2019-03-16 00:38:57 +0300

    More steps for coarse process

--- pproc.lua
+++ pproc.lua
@@ -13,8 +13,16 @@ ODS("Script~C0A 'pproc.lua'~C07 executed in thread~C0E "..thread_name.."~C07")
 
 function process_layer()
  if program_code == 'c' then
+   layer.exp_time = 4.5 -- this not affected???
+   -- more steps - erasing more edges
    layer:coarse()
-   -- layer:coarse(def_coarse_neighbors, 0, 1280, 1438, 2558) -- lower half of layer, for testing
+   layer:coarse()
+   layer:coarse()
+   -- lower half of layer, for testing
+    
+   -- layer:coarse(def_coarse_neighbors, 0, 1280, 1438, 2558) 
+   -- layer:coarse(def_coarse_neighbors, 0, 1280, 1438, 2558) 
+   -- layer:coarse(def_coarse_neighbors, 0, 1280, 1438, 2558) 
  else
    ODS(string.format("unsupported program code [%s]", program_code))    
  end   
